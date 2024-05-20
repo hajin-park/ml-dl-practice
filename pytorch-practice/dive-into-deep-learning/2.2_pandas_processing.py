ef49cff44887c89bdbe10c036ee784ad5d5c9c79
Hajin Park <hajin1819@gmail.com>
2024-05-20 14:34:31 -0700

    Finished dive into deep learning 2.2

--- pytorch-practice/dive-into-deep-learning/2.2_pandas_processing.py
+++ pytorch-practice/dive-into-deep-learning/2.2_pandas_processing.py
@@ -3,17 +3,31 @@ import pandas as pd
 import os
 
 
-os.makedirs(os.path.join("..", "data"), exist_ok=True)
-data_file = os.path.join("..", "data", "tiny_house.csv")
+os.makedirs(os.path.join("..", "..", "data"), exist_ok=True)
+data_file = os.path.join("..", "..", "data", "tiny_house.csv")
 
 with open(data_file, "w") as f:
     f.write(
-        """ NumRooms,RoofType,Price
-            NA,NA,127500
-            2,NA,106000
-            4,Slate,178100
-            NA,NA,140000"""
+        """NumRooms,RoofType,Price
+NA,NA,127500
+2,NA,106000
+4,Slate,178100
+NA,NA,140000"""
     )
 
 data = pd.read_csv(data_file)
-print(data)
+# print(data)
+
+# one-hot encoding to for categorical NAN values
+inputs, target = data.iloc[:, 0:2], data.iloc[:, 2]
+inputs = pd.get_dummies(inputs, dummy_na=True)
+# print(inputs)
+
+# mean value of column replaces numerical NAN values
+inputs = inputs.fillna(inputs.mean())
+# print(inputs)
+
+# convert entries into tensors
+X = torch.tensor(inputs.to_numpy(dtype=float))
+y = torch.tensor(target.to_numpy(dtype=float))
+print(X, y)
